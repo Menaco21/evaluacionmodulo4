ca9da440ebc2047052e85714368e1006564930cf
Menaco21 <alejandromenagahona@gmail.com>
2022-02-09 00:59:57 -0300

    Create juego.js

---
+++ juego.js
@@ -0,0 +1,6 @@
+let colors=["rgb(240, 14, 128)","rgb(67, 89, 244)","rgb(130, 199, 1)","rgb(87, 222, 111)","rgb(3, 2, 1)","rgb(69, 32, 255)"]
+let newColor = document.querySelectorAll(".square");
+for (var i=0 ; i < colors.length; i++){
+    square[i].style.backgroundColor;
+       }
+    
